6ed2c3058fc2e08eb3beec61bd1e6cf11ecfb943
ahm3d-hadi <hadiiae96@gmail.com>
2020-07-25 14:11:59 +0300

    Updates

---
+++ precache-manifest.11e1fc59f7490eb0749a412bd6565bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dada50ff9987ccb9da0a16290bcab0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0811cbed963d787fdd56",
+    "url": "/monsters-rolodex/static/css/main.6ad496c7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0811cbed963d787fdd56",
+    "url": "/monsters-rolodex/static/js/main.6d1017f0.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
